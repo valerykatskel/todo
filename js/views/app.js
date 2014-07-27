feff36e9050de4e21385e021c3c80b32ed47deb8
Valera Katskel <ne.vse.srazu@gmail.com>
2014-07-27 09:40:15 +0200

    Добавлены новые методы в главный файл приложения

--- js/views/app.js
+++ js/views/app.js
@@ -8,12 +8,22 @@ app.AppView = Backbone.View.extend({
 	// к существующему скелету приложения, имеющемуся в HTML.
 	el: '#todoapp',
 
-	// шаблон строки статистики в нижней части приложения.
+	// шаблон для строки статистики в нижней части приложения.
 	statsTemplate: _.template($('#stats-template').thml()),
+
+	// Новый код
+	// Делегированные события для создания новых задач и удаления завершенных.
+	events: {
+		'keypress #new-todo'		: 'createOnEnter',
+		'click #clear-completed'	: 'clearCompleted',
+		'click #toggle-all'			: 'toggleAllComplete'
+	}
 
 	// при инициализации мы делаем привязку
 	// к соответствующим событиям коллекции `Todos`
-	// при добавлении и изменении событий.
+	// когда ее элементы добавляются или изменяются. Мы начинаем
+	// с загрузки существующих задач, которые могли быть сохранены
+	// в локальное хранилище.
 	ititialize: function(){
 		this.allCheckbox = this.$('#toggle-all').get(0);
 		this.$input = this.$('#new-todo');
@@ -22,10 +32,37 @@ app.AppView = Backbone.View.extend({
 		this.listenTo(app.Todos, 'add', this.addOne);
 		this.listenTo(app.Todos, 'reset', this.addAll);
 
+		this.listenTo(app.Todos, 'change:completed', this.filterOne);
+		this.listenTo(app.Todos, 'filter', this.filterAll);
+		this.listenTo(app.Todos, 'all', this.render);
+		app.Todos.fetch();
 	},
 
-	// Добавление в список единственной задачи путем создания
-	// предствления для нее и добавления ее элемената в `<ul>`
+	// Повторное отображение приложения означает лишь обновление статистики.
+	// Остальная часть приложения не изменяется.
+	render: function(){
+		var completed = app.Todos.completed().length;
+		var remaining = app.Todos.remaining().length;
+		if(app.Todos.lenght){
+			this.$main.show();
+			this.$footer.show();
+			this.$footer.html(this.statsTemplate({
+				completed: completed,
+				remaining: remaining
+			}));
+			this.$('#filters li a')
+				.removeClass('selected')
+				.filter('[href="#/"' + (app.TodoFilter || '') + '"]')
+				.addClass('selected');
+		} else {
+			this.$main.hide();
+			this.$footer.hide();
+		}
+		this.allCheckbox.checked = !remaining;
+	}
+
+	// Добавление одной задачи в список путем создания представления для нее
+	// и добавления ее элемената в `<ul>`
 	addOne: function(todo){
    		var view = new app.TodoView({model: todo});
    		$('#todo-list').append(view.render().el);
@@ -35,5 +72,48 @@ app.AppView = Backbone.View.extend({
 	addAll: function(){
 		this.$('#todo-list').html('');
 		app.Todos.each(this.addOne, this);
+	},
+
+	filterOne: function(){
+		todo.trigger('visible');
+	},
+
+	filterAll: function(){
+		app.Todos.each(this.filterOne, this);
+	},
+
+	// Генерация атрибутов для новой задачи.
+	newAttributes: function(){
+		return{
+			title: this.$input.val().trim(),
+			order: app.todos.nextOrder(),
+			completed: false
+		}
+	},
+
+	// Создание новой задачи и ее сохранение
+	// в локальном хранилище при нажатии return
+	createOnEnter: function(event){
+		if(event.which !=== ENTER_KEY || !this.$input.val().trim()){
+			return;
+		}
+		app.Todos.create(this.newAttributes());
+		this.$input.val('');
+	},
+
+	// Удаление всех завершенных задач уничтожением их моделей
+	clearCompleted: function(){
+		_.invoke(app.Todos.completed(), 'destroy');
+		return false;
+	},
+
+	toggleAllComplete: function(){
+		var completed = this.allCheckbox.checked;
+		app.Todos.each(function(todo){
+			todo.save({
+				completed: 'completed'
+			})
+		})
 	}
+
 })
